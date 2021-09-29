2969b122f2f2e4fac93a7ff656edbb2640d8f06a
Allyson <allyson.gagne@gmail.com>
2021-09-29 05:12:14 -0400

    Update python_line_mlb.py

--- python_line_mlb.py
+++ python_line_mlb.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
 #data 
 # y1 = number of game wins by team with highest home run count
@@ -14,10 +13,12 @@ x = [2007,2006,2005,2004,2003,2002,2001,2000]
 plt.plot (x, y1, color="y", label='Team with Highest HR')
 plt.plot (x, y2, color= "c", label='Team with Highest OBP')
 plt.plot (x,y3, color="m", label= 'World Series Champ')
+
 #adapted from https://stackoverflow.com/questions/29096948/plot-a-vertical-line-using-matplotlib  (T. McKinney, 2020)
-plt.axvline(x=2002, color='b',ls='--', lw=2,label= 'Moneyball Season')
+plt.axvline(x=2002, color='b',ls='--', lw= 2,label= 'Moneyball Season')
 plt.legend()
 
+#graph labels 
 plt.title("Home Run Count (HR) and On Base Percentage (OBP) Impact on Game Wins")
 plt.xlabel("Year")
 plt.ylabel("Game Wins")
